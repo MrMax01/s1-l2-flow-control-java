6ed171515fc4604a3e5eb2426608d900c43357e7
MrMax01 <massimo.mauro00@gmail.com>
2023-10-03 14:52:54 +0200

    eserizioUno done

---
+++ EsercizioUno.java
@@ -0,0 +1,15 @@
+public class EsercizioUno {
+    public static void main(String[] args) {
+        System.out.println(stringaPariDispari("ciao"));
+        System.out.println(annoBisestile(2005));
+
+    }
+
+    public static boolean stringaPariDispari(String args){
+        
+        return args.length()%2==0;
+    }
+    public static  boolean annoBisestile(int year){
+        return year%4==0 || (year%100==0 && year%400==0);
+    }
+}
